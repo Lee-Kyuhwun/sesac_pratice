59f27e3d5d7fe2ec1a5b32d311e58d318b5e538b
이규훈 <wxyz0904@naver.com>
2023-06-12 17:26:01 +0900

    Feat: 6/12 공부한것
    6/12 공부한 것

--- backend/main.java
+++ backend/main.java
@@ -1,9 +1,21 @@
 package backend;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
+        String str1 = "Java Study";
 
-        System.out.println("연습파일");
-        
+        System.out.println("str1.toLowerCase() = " + str1.toLowerCase());
+        System.out.println("str1.toUpperCase() = " + str1.toUpperCase());
+
+        System.out.println("str1.replace(\"Study\",\"\") = " + str1.replace("Study","공부"));
+
+        System.out.println("str1.substring(0,5) = " + str1.substring(0,5));
+
+        String[] strArray = "abc/def-ghi jkl".split("/|-| ");
+        System.out.println(Arrays.toString(strArray)); //[abc, def, ghi, jkl]
+        System.out.println("str1.trim() = " + str1.trim());
     }
 }
---
+++ backend/test1.java
@@ -0,0 +1,7 @@
+package backend;
+
+public class test1 {
+    public static void main(String[] args) {
+
+    }
+}
