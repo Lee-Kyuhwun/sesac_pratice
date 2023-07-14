6c66867236adadc156a3812305f5841adf5ffb19
이규훈 <wxyz0904@naver.com>
2023-07-14 14:37:04 +0900

    Feat: 3장 세팅
    3장 기본 세팅

---
+++ w2/src/main/java/org/zerock/w2/controller/TodoRegisterController.java
@@ -0,0 +1,76 @@
+
+package org.zerock.w2.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.zerock.w2.dto.TodoDTO;
+import org.zerock.w2.service.TodoService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@WebServlet(name = "todoRegisterController", value = "/todo/register")
+@Log4j2
+public class TodoRegisterController extends HttpServlet {
+
+    private TodoService todoService = TodoService.INSTANCE;
+    private final DateTimeFormatter DATEFORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        log.info("/todo/register GET .......");
+
+        HttpSession session = req.getSession();
+
+        if(session.isNew()){
+            //기존에 세션아이디가 없는 새운 사용자
+            resp.sendRedirect("/login");
+            return;
+        }
+
+        if(session.getAttribute("loginInfo") == null){
+            log.info("로그인한 정보가 없느 사용자.");
+            resp.sendRedirect("/login");
+            return;
+        }
+
+
+        //세션 id가 있지만 해당 세션 컨텍스트에 loginInfo라는 이름으로 저장된 객체가 없는경우
+        if(session.getAttribute("loginInfo") ==null){
+            log.info("로그인한 정보가 없는 사용자");
+            resp.sendRedirect("/");
+
+        }
+
+
+
+        req.getRequestDispatcher("/WEB-INF/todo/register.jsp").forward(req,resp);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        TodoDTO todoDTO = TodoDTO.builder()
+                .title(req.getParameter("title"))
+                .dueDate(LocalDate.parse(req.getParameter("dueDate"),DATEFORMATTER ))
+                .build();
+
+        log.info("/todo/register POST...");
+        log.info(todoDTO);
+        try {
+            todoService.register(todoDTO);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        resp.sendRedirect("/todo/list");
+
+    }
+}
